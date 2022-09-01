cfb7e0385d13c274ecc923b91aa604bbbfc263ec
Elona Kajtazi <MY_NAME@example.com>
2022-09-01 22:35:14 +0200

    ....

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { quotes, authors, quoteType } from "./data";
+import { quotes, authors } from "./data";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,6 +13,7 @@ app.get("/", (req, res) => {
   <ul>
     <li><a href="/quotes">Quotes</a></li>
     <li><a href="/authors">Authors</a></li>
+    <li><a href="/random">Random Quote </a></li>
   </ul>`);
 });
 
@@ -21,20 +23,17 @@ app.get("/quotes", (req, res) => {
     return { ...quote, author };
   });
 
-  // let quotesToSend = quotes;
-  // console.log(Number(req.query.quantity))
-  // if (req.query.age) {
-  //   quotesToSend = quotesToSend.filter(
-  //     (quote) => quote.age === Number(req.query.age)
-  //   );
-  // }
-  // if (req.query.quote) {
-  //   quotesToSend = quotesToSend.filter((quote) =>
-  //     //@ts-ignore
-  //     quote.quote.toLowerCase().includes(req.query.quote.toLowerCase())
-  //   );
-  // }
   res.send(quotesToSend);
+});
+
+app.get("/quotes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const match = quotes.find((quote) => quote.id === id);
+  if (match) {
+    res.send(match);
+  } else {
+    res.status(404).send({ error: "Quote not found!" });
+  }
 });
 
 app.post("/quotes", (req, res) => {
@@ -89,6 +88,7 @@ app.patch("/quotes/:id", (req, res) => {
     res.status(404).send({ error: "Quote not found" });
   }
 });
+
 app.get("/authors", (req, res) => {
   // let ownersToSend = owners.map(owner => {
   //   const foundDogs = dogs.filter(dog => dog.ownerId === owner.id)
@@ -103,6 +103,7 @@ app.get("/authors", (req, res) => {
 
   res.send(authorsToSend);
 });
+
 app.post("/authors", (req, res) => {
   let errors: string[] = [];
   if (typeof req.body.firstName !== "string") {
@@ -143,6 +144,7 @@ app.delete("/authors/:id", (req, res) => {
     res.status(404).send({ error: "Author not found" });
   }
 });
+
 app.patch("/authors/:id", (req, res) => {
   let id = Number(req.params.id);
   let match = authors.find((author) => author.id === id);
@@ -164,55 +166,12 @@ app.patch("/authors/:id", (req, res) => {
     res.status(404).send({ error: "Author not found" });
   }
 });
-// app.post("/quotes", (req, res) => {
-//   let errors: string[] = [];
-//   if (typeof req.body.firstName !== "string") {
-//     errors.push("FirstName not provided or not a string.");
-//   }
-//   if (typeof req.body.lastName !== "string") {
-//     errors.push("LastName not provided or not a string.");
-//   }
-//   if (typeof req.body.age !== "number") {
-//     errors.push("age not provided or not a number.");
-//   }
-//   if (typeof req.body.image !== "string") {
-//     errors.push("image not provided or not a valid url.");
-//   }
-//   if (typeof req.body.quote !== "string") {
-//     errors.push("Quote not provided or not a string.");
-//   }
-//   if (errors.length === 0) {
-//     const newQuote = {
-//       id: quotes[quotes.length - 1].id + 1,
-//       firstName: req.body.firstName,
-//       lastName: req.body.lastName,
-//       age: req.body.age,
-//       image: req.body.image,
-//       quote: req.body.quote,
-//     };
-//     quotes.push(newQuote);
-//     res.send(newQuote);
-//   } else {
-//     res.status(400).send({ errors: errors });
-//   }
-// });
-// app.get("/quotes", (req, res) => {
-//   res.send(quotes);
-// });
-// app.get("/quotes/:id", (req, res) => {
-//   const id = Number(req.params.id);
-//   const match = quotes.find((quote) => quote.id === id);
-//   if (match) {
-//     res.send(match);
-//   } else {
-//     res.status(404).send({ error: "Quote not found!" });
-//   }
-// });
 
-// app.get("/random", (req, res) => {
-//   const randomIndex = Math.floor(Math.random() * quotes.length);
-//   res.send(quotes[randomIndex]);
-// });
+app.get("/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  res.send(quotes[randomIndex]);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
